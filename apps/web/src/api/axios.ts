a8f55ae740c6ae00616b1c891b06ca484dd24c4b
terminalink0-cloud <terminalink0@gmail.com>
2026-09-08 00:22:41 +0800

    Use VITE_API_URL for API base URL

--- apps/web/src/api/axios.ts
+++ apps/web/src/api/axios.ts
@@ -1,13 +1,8 @@
 import axios from "axios";
 
-
 const api = axios.create({
-  baseURL: "http://localhost:3001",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001",
 });
-
 
 api.interceptors.request.use(
   (config) => {
