88636c254344ac83212beb2d428dc14184027f15
terminalink0-cloud <terminalink0@gmail.com>
2026-09-08 00:49:37 +0800

    Use VITE_API_URL in client and vehicles API

--- apps/web/src/api/client.ts
+++ apps/web/src/api/client.ts
@@ -1,17 +1,15 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:3001",
+const client = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001",
 });
 
-api.interceptors.request.use((config) => {
+client.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
-export default api;
+export default client;
--- apps/web/src/api/vehicles.api.ts
+++ apps/web/src/api/vehicles.api.ts
@@ -1,153 +1,26 @@
-import axios from "axios";
+import client from "./client"; // or "./axios" depending on which shared instance you use
 
-
-const API_URL =
-  "http://localhost:3001/vehicles";
-
-
-
-const authHeaders = () => {
-
-  const token =
-    localStorage.getItem("token");
-
-
-  return {
-
-    headers: {
-
-      Authorization:
-        `Bearer ${token}`
-
-    }
-
-  };
-
-};
-
-
-
-
-
-
-export async function getVehicles(){
-
-
-  const res =
-    await axios.get(
-
-      API_URL,
-
-      authHeaders()
-
-    );
-
-
+export async function getVehicles() {
+  const res = await client.get("/vehicles");
   return res.data;
-
 }
 
-
-
-
-
-
-
-export async function getVehicle(
-  id:string
-){
-
-
-  const res =
-    await axios.get(
-
-      `${API_URL}/${id}`,
-
-      authHeaders()
-
-    );
-
-
+export async function getVehicle(id: string) {
+  const res = await client.get(`/vehicles/${id}`);
   return res.data;
-
 }
 
-
-
-
-
-
-
-export async function createVehicle(
-  data:any
-){
-
-
-  const res =
-    await axios.post(
-
-      API_URL,
-
-      data,
-
-      authHeaders()
-
-    );
-
-
+export async function createVehicle(data: any) {
+  const res = await client.post("/vehicles", data);
   return res.data;
-
 }
 
-
-
-
-
-
-
-export async function updateVehicle(
-  id:string,
-  data:any
-){
-
-
-  const res =
-    await axios.patch(
-
-      `${API_URL}/${id}`,
-
-      data,
-
-      authHeaders()
-
-    );
-
-
+export async function updateVehicle(id: string, data: any) {
+  const res = await client.patch(`/vehicles/${id}`, data);
   return res.data;
-
 }
 
-
-
-
-
-
-
-export async function deleteVehicle(
-  id:string
-){
-
-
-  const res =
-    await axios.delete(
-
-      `${API_URL}/${id}`,
-
-      authHeaders()
-
-    );
-
-
+export async function deleteVehicle(id: string) {
+  const res = await client.delete(`/vehicles/${id}`);
   return res.data;
-
 }
